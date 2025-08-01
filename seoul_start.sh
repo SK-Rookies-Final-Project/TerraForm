b6b6f4d7fd508a725d7dbeee23a67a3b11f2c029
J-millar99 <wlwogus33@gmail.com>
2025-08-01 12:26:55 +0900

    refactoring ansible of seoul region

--- seoul_start.sh
+++ seoul_start.sh
@@ -3,7 +3,7 @@ terraform init
 terraform plan -var-file="../common/terraform.tfvars"
 terraform apply -auto-approve -var-file="../common/terraform.tfvars"
 
-cd ../ansible-ubuntu
+cd ansible
 sh ip_setup.sh
 
 ansible-playbook -i inventory.ini packages.yml
