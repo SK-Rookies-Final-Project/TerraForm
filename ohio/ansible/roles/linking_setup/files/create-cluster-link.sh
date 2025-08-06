22951073fad604833dff7cc67d0cf904e06f6b5d
lemongza <92498054+lemongza@users.noreply.github.com>
2025-08-06 13:08:48 +0900

    경로 수정

--- ohio/ansible/roles/linking_setup/files/create-cluster-link.sh
+++ ohio/ansible/roles/linking_setup/files/create-cluster-link.sh
@@ -2,7 +2,7 @@
 
 # Kafka Cluster Link 생성 스크립트 (브로커 1번)
 /bin/kafka-cluster-links \
-  --bootstrap-server ec2-3-35-199-84.ap-northeast-2.compute.amazonaws.com:29092 \
+  --bootstrap-server ec2-18-189-168-183.us-east-2.compute.amazonaws.com:29092 \
   --create \
   --link link-link \
-  --config-file /engn/confluent/link-link.config
+  --config-file /home/appuser/link-link.config
